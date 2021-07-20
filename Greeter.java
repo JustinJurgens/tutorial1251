79e0bd56337b6f5a0a92b2fd8c4f8ca06130f052
Justin <jjurgens1243@gmail.com>
2021-07-21 11:33:21 +1200

    Fixed bug again

--- Greeter.java
+++ Greeter.java
@@ -20,6 +20,7 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
+    String test = "Test";
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
